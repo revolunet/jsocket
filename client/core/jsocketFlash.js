265ea6ab7171d1f11d1313f71ed944c4d50828e0
Remi MAREK <bydooweedoo@gmail.com>
2010-02-17 11:19:41 +0100

    Client: Ajout variable javascript jsocketBridgeDomain

--- client/core/jsocketFlash.js
+++ client/core/jsocketFlash.js
@@ -1,7 +1,7 @@
 /**
  * Includes swf for flash socket bridge
  */
-var jsocketFlashHtml = '<embed height="300" width="1000" flashvars="scope=jsocketCore" allowscriptaccess="always" quality="low" bgcolor="#ffffff" name="socketBridge" id="socketBridge" style="" src="' + jsocketDomain + 'ext/jsocketBridge.swf" type="application/x-shockwave-flash">';
+var jsocketFlashHtml = '<embed height="300" width="1000" flashvars="scope=jsocketCore" allowscriptaccess="always" quality="low" bgcolor="#ffffff" name="socketBridge" id="socketBridge" style="" src="' + jsocketBridgeDomain + '" type="application/x-shockwave-flash">';
 document.getElementById('flashcontent').innerHTML = jsocketFlashHtml;
 document.getElementById('flashcontent').style.visibility = 'hidden';
 document.getElementById('flashcontent').style.position = 'absolute';
