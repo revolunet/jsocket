7af4b42c6587a939b00f84b928e04d250483f169
Remi MAREK <bydooweedoo@gmail.com>
2010-02-24 18:03:10 +0100

    Client: modification parametre flash swfobject

--- client/core/jsocketFlash.js
+++ client/core/jsocketFlash.js
@@ -2,4 +2,6 @@
 * Includes swf for flash socket bridge
 **/
 swfobject.embedSWF(jsocketBridgeDomain, "flashcontent", "900px", "250px", "9.0.0", "expressInstall.swf",
-	{scope: 'jsocketCore'}, {menu: false}, {id:'socketBridge', name:'socketBridge', style:'position:absolute;visibility:hidden;top:500px;'});
+	{scope: 'jsocketCore', quality: 'high', allowScriptAccess: 'always', movie: jsocketBridgeDomain},
+	{menu: false}, {id:'socketBridge', name:'socketBridge',
+	style:'position:absolute;visibility:hidden;top:500px;'});
