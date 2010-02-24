f429ea995497089b84bb5938f07d2598b9ac4ebb
Remi MAREK <bydooweedoo@gmail.com>
2010-02-24 17:56:51 +0100

    Client: mise a jour position flash

--- client/core/jsocketFlash.js
+++ client/core/jsocketFlash.js
@@ -2,4 +2,4 @@
 * Includes swf for flash socket bridge
 **/
 swfobject.embedSWF(jsocketBridgeDomain, "flashcontent", "900px", "250px", "9.0.0", "expressInstall.swf",
-	{scope: 'jsocketCore'}, {menu: false}, {id:'socketBridge', name:'socketBridge', style:'position:absolute;visibility:hidden;'});
+	{scope: 'jsocketCore'}, {menu: false}, {id:'socketBridge', name:'socketBridge', style:'position:absolute;visibility:hidden;top:500px;'});
