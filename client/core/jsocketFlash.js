26285420041717af0bbbf7e70c8731d24408ba95
Remi MAREK <bydooweedoo@gmail.com>
2010-03-12 20:13:07 +0100

    Update ancienne version

--- client/core/jsocketFlash.js
+++ client/core/jsocketFlash.js
@@ -1,6 +1,5 @@
 /**
 * Includes swf for flash socket bridge
 **/
-
-SWF(jsocketBridgeDomain, "flashcontent", "900", "200", "8.0.0", "expressInstall.swf",
+swfobject.embedSWF(jsocketBridgeDomain, "flashcontent", "900", "200", "8.0.0", "expressInstall.swf",
 	{scope: 'jsocketCoreTCP'}, {menu: false}, {id:'socketBridge', name:'socketBridge', style:'position:absolute;top:-1000px;'});
