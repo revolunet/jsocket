eb9a597bd776ee25273e5f29a4a9702977a3293f
Remi MAREK <bydooweedoo@gmail.com>
2010-04-09 17:09:58 +0200

    BugFix swfobject load

--- client/core/jsocketFlash.js
+++ client/core/jsocketFlash.js
@@ -1,5 +1,11 @@
 /**
 * Includes swf for flash socket bridge
 **/
-swfobject.embedSWF(jsocketBridgeDomain, "flashcontent", "900", "200", "8.0.0", "expressInstall.swf",
-	{scope: 'jsocketCoreTCP'}, {menu: false}, {id:'socketBridge', name:'socketBridge', style:'position:absolute;top:-1000px;'});
+function createSwf() {
+	if (typeof swfobject != 'undefined') {
+		swfobject.embedSWF(jsocketBridgeDomain, "flashcontent", "900", "200", "8.0.0", "expressInstall.swf",
+			{scope: 'jsocketCoreTCP'}, {menu: false}, {id:'socketBridge', name:'socketBridge', style:'position:absolute;top:-1000px;'});
+	} else {
+		setTimeout('createSwf()', 200);
+	}
+}
