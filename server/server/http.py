b805116c3afc78b3b136bc453f942b634f119e73
bydooweedoo <bydooweedoo@beewoo.localdomain>
2010-04-27 01:51:34 +0200

    Add client/client.py

--- server/server/http.py
+++ server/server/http.py
@@ -11,17 +11,10 @@ from log.logger import Log
 from config.settings import SETTINGS
 
 class ServerHTTP(threading.Thread):
-	def __init__(self, room, squeue, rqueue, client_list, http_list, session):
-		self.__host = SETTINGS.SERVER_HOST
-		self.__room = room
-		self.__rqueue = rqueue
-		self.__squeue = squeue
-		self.client_list = client_list
-		self.http_list = http_list
-		self.session = session
+	def __init__(self):
 		threading.Thread.__init__(self)
 
 	def run(self):
-		Log().add("[+] HTTP Server launched on %s:%d" % (self.__host, SETTINGS.SERVER_HTTP_PORT), "green")
-		client = server.Site(ClientHTTP(self.__room, self.__rqueue, self.__squeue, self.http_list, self.session))
+		Log().add("[+] HTTP Server launched on %s:%d" % (SETTINGS.SERVER_HOST, SETTINGS.SERVER_HTTP_PORT), "green")
+		client = server.Site(ClientHTTP())
 		reactor.listenTCP(SETTINGS.SERVER_HTTP_PORT, client, interface=SETTINGS.SERVER_HOST)
