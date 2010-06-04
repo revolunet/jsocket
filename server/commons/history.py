1c62ffbc940a4d51651fbf58057f27b5805f71ed
Fabien Reboia <srounet@gmail.com>
2010-06-04 18:37:38 +0200

    History documentation

--- server/commons/history.py
+++ server/commons/history.py
@@ -1,10 +1,18 @@
 import time
 
 class History(object):
+	"""
+	Garde en mémoire les messages d'un channel
+	"""
+	
 	def __init__(self):
 		self.__history = []
 		
 	def add(self, uid, json):
+		"""
+		Ajout un message a l'historique du channel
+		"""
+		
 		self.__history.append({
 			'time': time.time(),
 			'uid': uid,
@@ -12,9 +20,17 @@ class History(object):
 		})
 	
 	def get(self):
+		"""
+		Renvoie la liste des messages du channel
+		"""
+		
 		return self.__history
 		
 	def get_from(self, start, end):
+		"""
+		Renvoie la liste des messages du channel compris entre start et end ( microtime )
+		"""
+		
 		history = []
 		for h in self.__history:
 			if h.time <= start and h.time >= end:
@@ -22,4 +38,8 @@ class History(object):
 		return history
 		
 	def flush(self):
+		"""
+		Reset la taille de l'historique
+		"""
+		
 		self.__history = []
