6aef81ebae870a1dc7bfecc9796b034bec7a6ea7
Fabien Reboia <srounet@gmail.com>
2010-06-04 17:56:28 +0200

    + fichier history

---
+++ server/commons/history.py
@@ -0,0 +1,22 @@
+import time
+
+class History(object):
+	def __init__(self):
+		self.__history = []
+		
+	def add(self, uid, json):
+		self.__history.append({
+			'time': time.time(),
+			'uid': uid,
+			'json': json
+		})
+	
+	def get(self):
+		return self.__history
+		
+	def get_from(self, start, end):
+		history = []
+		for h in self.__history:
+			if h.time <= start and h.time >= end:
+				history.append(h)
+		return history
