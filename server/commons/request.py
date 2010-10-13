b0ec34897051026e894045096758c03a530b85f4
revolunet <julien@bouquillon.com>
2010-10-13 17:27:11 +0200

    5sec timeout for disconnect callback

--- server/commons/request.py
+++ server/commons/request.py
@@ -2,7 +2,6 @@ import urllib
 import threading
 from log.logger import Log
 
-
 class Request(threading.Thread):
     def __init__(self, vhost, uid, status):
         self.vhost = vhost
@@ -12,6 +11,9 @@ class Request(threading.Thread):
 
     def run(self):
         if self.vhost and self.uid and self.status:
+            import socket
+            socket.setdefaulttimeout(5)
+
             Log().add("[+] Channel : send status %s for %s to %s" % (self.status, self.uid, self.vhost), 'blue')
             try:
                 urllib.urlopen("%s/jsocket?uid=%s&status=%s" % (self.vhost, self.uid, self.status))
@@ -20,5 +22,7 @@ class Request(threading.Thread):
                     Log().add("[!] cannot send http status : %s" % e.reason, 'red')
                 elif hasattr(e, 'code'):
                     Log().add("[!] cannot send http status : %s" % e.code, 'red')
+                elif hasattr(e, 'strerror'):
+                    Log().add("[!] cannot send http status : %s" % e.strerror, 'red')
                 else:
                     Log().add("[!] cannot send http status", 'red')
