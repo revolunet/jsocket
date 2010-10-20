514a848264c002869cb63c5d6d2dcde428388320
revolunet <julien@bouquillon.com>
2010-10-20 14:16:51 +0200

    flash.js : add allowscriptaccess param for crossdomain embeds

--- client/code/flash.js
+++ client/code/flash.js
@@ -4,8 +4,8 @@
 function createSwf() {
 	if (typeof swfobject != 'undefined' && typeof jsocket.core.tcp != 'undefined') {
 		swfobject.embedSWF(jsocketBridgeDomain, "flashcontent", "900", "200", "8.0.0", "expressInstall.swf",
-						   {scope: 'jsocket.core.tcp'}, {menu: false},
-						   {id:'socketBridge', name:'socketBridge', style:'position:absolute;top:-1000px;'});
+						   {scope: 'jsocket.core.tcp', AllowScriptAccess:'always'}, {menu: false, AllowScriptAccess:'always'},
+						   {id:'socketBridge', name:'socketBridge', style:'position:absolute;top:-1000px;', AllowScriptAccess:'always'});
 	} else {
 		setTimeout('createSwf()', 200);
 	}
--- client/public/jsocket.js
+++ client/public/jsocket.js
@@ -2175,8 +2175,8 @@ jsocket.api.register('myApplicationName', myApplication);
 function createSwf() {
 	if (typeof swfobject != 'undefined' && typeof jsocket.core.tcp != 'undefined') {
 		swfobject.embedSWF(jsocketBridgeDomain, "flashcontent", "900", "200", "8.0.0", "expressInstall.swf",
-						   {scope: 'jsocket.core.tcp'}, {menu: false},
-						   {id:'socketBridge', name:'socketBridge', style:'position:absolute;top:-1000px;'});
+						   {scope: 'jsocket.core.tcp', AllowScriptAccess:'always'}, {menu: false, AllowScriptAccess:'always'},
+						   {id:'socketBridge', name:'socketBridge', style:'position:absolute;top:-1000px;', AllowScriptAccess:'always'});
 	} else {
 		setTimeout('createSwf()', 200);
 	}
